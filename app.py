c82dc385b7b4918c6021dbcb9f8b22ff6b220ad1
AnujPanthri <50690518+AnujPanthri@users.noreply.github.com>
2021-01-31 20:59:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,43 +27,9 @@ def home():
     return "<h2>hi dear</h2>"
 
 
-@app.route('/c_d/',methods=['POST'])
+@app.route('/cd/',methods=['POST'])
 def classifier():
-    data=request.form.to_dict(flat=False)
-    length=len(data)
-    print("len",length)
-    allimg=[]
-    for i in range(length):
-        s=str(i)
-        im_b64 = data[s][0] 
-        im_b64=bytes(im_b64,'ascii')
-        de=base64.b64decode(im_b64)
-        buf=io.BytesIO(de)
-        img=Image.open(buf)
-        temparr=array(img)
-        temparr=temparr.reshape([-1,150,150,3])
-        print("len:",temparr.shape)
-        if i==0:
-            allimg=temparr
-        else:
-            allimg=np.concatenate((allimg,temparr))
-    print("all",allimg.shape)
-    allimg = allimg.astype('float32')
-    allimg = allimg/255
-    predictions=np.array([[2],[3]])
-    result=[]
-    predictions=np.ndarray.flatten(predictions)
-    print(predictions.shape)
-    i=0
     
-    for i in range(len(predictions)):
-        n=predictions[i]
-        n=str(n)
-        print("value:",n)
-
-        #print("see:",color_dict[n])
-        result.append({'dog':n})
-    #print(result)
     return jsonify("result2")
 
 if __name__ == '__main__':
