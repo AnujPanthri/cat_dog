a656009052cffc0011dadbdcb028d195ea97887c
AnujPanthri <50690518+AnujPanthri@users.noreply.github.com>
2021-01-31 21:06:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,4 @@ def classifier():
     
     return jsonify("result2")
 
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+app.run()
