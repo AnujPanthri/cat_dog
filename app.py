d61af09c72b1a3f6af11a3e0834bd8cdfb7ca370
AnujPanthri <50690518+AnujPanthri@users.noreply.github.com>
2021-01-31 21:36:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,32 +3,30 @@ import flask
 from flask import request, jsonify
 # Importing Tensorflow
 import tensorflow as tf
-import base64
-import io
-from PIL import Image
 
 #print(tf.__version__)
 
 """## Loading Trained Model"""
 # Recreate the exact same model, including its weights and the optimizer
-model = tf.keras.models.load_model('imgmodel.h5') 
+# model = tf.keras.models.load_model('colormodel_trained.h5') 
 
 """## Initializing Color Classes for Prediction"""
 
 # Mapping the Color Index with the respective 11 Classes (More Explained in RGB Color Classifier: Part 1)
 
 app=flask.Flask(__name__)
-#app.config["DEBUG"]=True
+app.config["DEBUG"]=True
 #predicting from loaded trained_model
 
 
 @app.route('/')
 def home():
-    return "<h2>hi dear</h2>"
+    return "<h1>working</h1>"
 
 
 @app.route('/api/',methods=['POST'])
 def classifier():
+    print("hey!")
     return jsonify("resultfromdog")
 
 
