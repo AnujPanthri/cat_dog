f0c0218e2879980fbbb04ad6c485ac11ba482d5c
AnujPanthri <50690518+AnujPanthri@users.noreply.github.com>
2021-01-31 19:34:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def classifier():
         n=predictions[i]
         #print("see:",color_dict[n])
         result.append({'dog':n})
-    return jsonify(predictions)
+    return predictions
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
