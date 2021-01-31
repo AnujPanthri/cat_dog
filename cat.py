c9c4ce29291351f8ff0ed5411ca8f7bbea826c9f
AnujPanthri <50690518+AnujPanthri@users.noreply.github.com>
2021-01-31 17:42:18 +0530

    Update cat.py

--- cat.py
+++ cat.py
@@ -49,4 +49,6 @@ def classifier():
     
     #img.show()
     
-app.run() 
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
