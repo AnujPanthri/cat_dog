ebb5f22cf7207b3c9bd8cfe4b47de03d75631c83
AnujPanthri <50690518+AnujPanthri@users.noreply.github.com>
2021-02-01 12:02:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,39 @@ def home():
 @app.route('/api/',methods=['POST'])
 def predict_color():
     data=request.get_json(force=True)
-    print("wo")
+    length=len(data)
+    print("len",length)
+    allimg=[]
+    for i in range(length):
+        s=str(i)
+        im_b64 = data[s][0] 
+        im_b64=bytes(im_b64,'ascii')
+        de=base64.b64decode(im_b64)
+        buf=io.BytesIO(de)
+        img=Image.open(buf)
+        temparr=array(img)
+        temparr=temparr.reshape([-1,150,150,3])
+        print("len:",temparr.shape)
+        if i==0:
+            allimg=temparr
+        else:
+            allimg=np.concatenate((allimg,temparr))
+    print("all",allimg.shape)
+    allimg = allimg.astype('float32')
+    allimg = allimg/255
+    predictions=model.predict(allimg)
+    result=[]
+    predictions=np.ndarray.flatten(predictions)
+    print(predictions.shape)
+    i=0
+    
+    for i in range(len(predictions)):
+        n=predictions[i]
+        n=str(n)
+        print("value:",n)
+
+        #print("see:",color_dict[n])
+        result.append({'dog':n})
     return jsonify(data)
 
 if __name__ == '__main__' :
